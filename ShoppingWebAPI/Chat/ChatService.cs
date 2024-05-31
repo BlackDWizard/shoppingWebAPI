05dfc714f1a986233a42b8cd36fa70377449ce2d
JonathanHuang <jonathan520419@gmail.com>
2024-05-31 16:42:49 +0800

    ADD Simple Socket Service

---
+++ ShoppingWebAPI/Chat/ChatService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace ShoppingWebAPI.Chat
+{
+    public class ChatService
+    {
+        private readonly List<WebSocket> _sockets = new();
+
+        public async Task HandleWebSocketConnection(WebSocket socket)
+        {
+            _sockets.Add(socket);
+            var buffer = new byte[1024 * 2];
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), default);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, default);
+                    break;
+                }
+
+                foreach (var s in _sockets)
+                {
+                    var messageString = Encoding.UTF8.GetString(buffer[..result.Count]);
+                    var bufferSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(messageString + "\t" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")));
+                    await s.SendAsync(bufferSend, WebSocketMessageType.Text, true, default);
+                }
+            }
+            _sockets.Remove(socket);
+        }
+    }
+}
--- ShoppingWebAPI/Program.cs
+++ ShoppingWebAPI/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region WebSocket
+//WebSocket
+builder.Services.AddSingleton<ShoppingWebAPI.Chat.ChatService>();
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +20,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+#region WebSocket
+app.UseWebSockets();
+
+app.MapGet("/Chat/", async (HttpContext context, ShoppingWebAPI.Chat.ChatService chatService) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        await chatService.HandleWebSocketConnection(webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Expected a WebSocket request");
+    }
+});
+#endregion
 
 app.UseHttpsRedirection();
 
